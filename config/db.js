b6070625a4b5e28a30d15ba7efb88defac30a01d
akrammlh02 <akramgaming28@gmail.com>
2025-11-12 21:43:14 +0100

    Initial commit with full project

--- config/db.js
+++ config/db.js
@@ -4,14 +4,18 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const conn = mysql.createConnection({
-  host : process.env.DB_HOST,
-  user : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_NAME
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: process.env.DB_SSL_CA
+  }
 })
 
-conn.connect((err)=>{
-  if(err) throw err;
+conn.connect((err) => {
+  if (err) throw err;
   console.log('Connected to DevAcademy DB.');
 })
 
